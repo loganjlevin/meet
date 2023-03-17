477d5320935053026ebd61e4d2255366a0793c78
Logan Levin <loganjlevin@gmail.com>
2023-03-17 14:31:26 -0700

    implemented oop alerts

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
-  handleInputChange = (event) => {
-    const value = event.target.value;
-    const suggestions = this.props.locations.filter((location) => {
-      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
-    });
-    this.setState({ query: value, suggestions });
-  };
-  handleItemClicked = (suggestion) => {
-    this.setState({
-      query: suggestion,
-      showSuggestions: false,
-    });
-    this.props.updateEvents(suggestion);
-  };
   state = {
     query: '',
     suggestions: [],
     showSuggestions: undefined,
+    infoText: '',
   };
+
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({ showSuggestions: true });
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText:
+          'We can not find the city you are looking for. Please try another city.',
+      });
+    } else {
+      this.setState({
+        query: value,
+        suggestions,
+        infoText: '',
+      });
+    }
+  };
+  handleItemClicked = (suggestion) => {
+    this.setState({
+      query: suggestion,
+      suggestions: [],
+      showSuggestions: false,
+      infoText: '',
+    });
+    this.props.updateEvents(suggestion);
+  };
+
   render() {
     return (
       <div className="CitySearch">
+        <p>Search for a city...</p>
+        <InfoAlert text={this.state.infoText} />
         <input
           type="text"
           className="city"
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
     number: this.props.numberOfEvents,
+    errorText: '',
   };
 
   handleInputChange = (event) => {
     const value = event.target.value;
     this.setState({ number: value });
-    this.props.updateNumEvents(value);
+    if (value < 1) {
+      this.setState({
+        errorText: 'Select number from 1 to 32',
+        number: 1,
+      });
+    } else if (value > 32) {
+      this.setState({
+        errorText: 'Select number from 1 to 32',
+        number: 32,
+      });
+    } else {
+      this.setState({
+        errorText: '',
+        number: value,
+      });
+      this.props.updateNumEvents(value);
+    }
   };
 
   render() {
     return (
-      <div className="NumberOfEvents">
+      <div className="numberOfEvents">
+        <p>How many events would you like to see?</p>
         <input
           type="number"
           className="num-events"
           value={this.state.number}
           onChange={this.handleInputChange}
         />
+        <ErrorAlert text={this.state.errorText} />
       </div>
     );
   }
